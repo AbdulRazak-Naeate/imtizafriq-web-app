adac70c00cb7c9ee4dba9d10e842636ff313c4c7
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2021-08-30 04:36:11 +0000

    change server index.js

--- server/index.js
+++ server/index.js
@@ -6,16 +6,43 @@ const dotenv = require('dotenv');
 const app = express();
 const cors = require('cors');
  
+
+//Import Routes
+const productsRoute = require('./routes/products');
+const userRoute     = require('./routes/auth');
+const storeRoute    = require('./routes/stores');
+const storeCategory = require('./routes/storecategories');
+const requests      = require('./routes/requests');
+const carts         = require('./routes/carts');
+
+dotenv.config();
 //MiddleWare
-app.use(express.json());
+app.use(cors()); // package to allow connections from outisde domains
+app.use(express.json()); //body-parser alternate
 app.use(pino);
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+//Route MiddleWares
+app.use('/uploads',express.static('./uploads'));//making uploads folder accessible
+app.use('/api/user',    userRoute);
+app.use('/api/products',productsRoute);
+app.use('/api/stores',  storeRoute);
+app.use('/api/storecategory', storeCategory);
+app.use('/api/requests',requests);
+app.use('/api/carts',carts);
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+//Home Routes
+ app.get('/',(req,res)=>{
+     res.send('We are on home')
+ })
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true ,useUnifiedTopology: true }, ()=>{
+    console.log('connected to DB!')
+})
+
+//Start lestening to the server
+app.set('PORT',3001 ||process.env.PORT);
+app.listen(app.get('PORT'),()=>{
+    console.log(`Server is running on ${app.get('PORT')}`);
+});
