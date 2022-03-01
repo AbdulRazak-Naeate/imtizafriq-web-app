00e6d32189e1ee656b3459faf2eef57d6f6fae2c
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-01 20:05:41 +0000

    Change package.json , server/index.js

--- server/index.js
+++ server/index.js
@@ -94,10 +94,10 @@ mongoose.connect(process.env.DB_COMMUNITY_CON, options)
      console.log(process.env.NODE_ENV)
      if (process.env.NODE_ENV === 'production') {
         // Serve any static files
-        app.use(express.static(path.join(__dirname, 'build')));
+        app.use(express.static(path.join(__dirname, '../build')));
       // Handle React routing, return all requests to React app
         app.get('*', function(req, res) {
-          res.sendFile(path.join(__dirname, 'build', 'index.html'));
+          res.sendFile(path.join(__dirname, '../build', 'index.html'));
         });
       }  
 //Start lestening to the server
