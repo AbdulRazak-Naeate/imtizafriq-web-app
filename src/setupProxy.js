124a126b6c072e7642882af8eace7d41a03d4c35
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-01 06:33:10 +0000

    made changes to setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app) {
-  app.use(
-    createProxyMiddleware("/api", { target: "http://localhost:3000",changeOrigin:true })
-  );
-};
+ module.exports = function(app){
+   app.use(createProxyMiddleware('/flask/**', { target: 'localhost:5000' }))
+  
+  }
+   
