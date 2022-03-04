c5f54c8c3d44a90d0e50e6fd27cb8cce84bae91d
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-04 06:38:24 +0000

    make it better"

--- server/index.js
+++ server/index.js
@@ -91,14 +91,14 @@ mongoose.connect(process.env.DB_CONNECTION, options)
     db.on('error',err =>{
         console.error('connection eror:',err)
      })
-  ///   if (process.env.NODE_ENV === 'production') {
+     if (process.env.NODE_ENV  === 'production') {
         // Serve any static files
         app.use(express.static(path.resolve(__dirname, '../client/build')));
       // Handle React routing, return all requests to React app
         app.get('*', function(req, res) {
           res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
         });
-   //   }  
+      }  
 //Start lestening to the server
 app.set('PORT',  process.env.PORT || 3001);
 
