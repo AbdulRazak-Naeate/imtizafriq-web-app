31b3ea4c16da217d06da3896bfbe05a0c8f11b2f
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-04 06:13:27 +0000

    chnages path.join to path.resolve in server/index.js"

--- server/index.js
+++ server/index.js
@@ -93,10 +93,10 @@ mongoose.connect(process.env.DB_CONNECTION, options)
      })
      if (process.env.NODE_ENV === 'production') {
         // Serve any static files
-        app.use(express.static(path.join(__dirname, '../client/build')));
+        app.use(express.static(path.resolve(__dirname, '../client/build')));
       // Handle React routing, return all requests to React app
         app.get('*', function(req, res) {
-          res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+          res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
         });
       }  
 //Start lestening to the server
