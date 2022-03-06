ab398c669e7b5f00a28892f40f383b57d54e9477
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-06 11:01:25 +0000

    form data erro fixing"

--- server/routes/products.js
+++ server/routes/products.js
@@ -36,16 +36,7 @@ router.post('/',verify, async(req,res)=>{
 
     //Validation
     const {error} = productValidation(req.body);
-    let form = new multiparty.Form();
 
-    form.parse(req, function(err, fields, files) {
-        
-       Object.keys(fields).forEach(function(name) {
-            console.log('got field named ' + name);
-            return res.json({data:name})
-
-        });
-    });
     if (error) return  res.json({status:400,message:error.details[0].message,d:req.body});
 
     //check if product name already exist
