b1d2ddabdcffed0f08c26e523ec582e36d83a29a
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-10 19:09:22 +0000

    mak eit better

--- server/index.js
+++ server/index.js
@@ -81,8 +81,9 @@ app.use('/api/contacts',contactsRoute);
      res.send('IntizAfriq Web App  version 0.1')
  })
 const options={ useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false }
+
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, options)
+mongoose.connect(process.env.DB_CONNECTION,options)
 
     const db = mongoose.connection
     db.once('open', _ =>{
