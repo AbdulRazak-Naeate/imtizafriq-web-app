30577d169bf229d542e33c696422e420e78c47b3
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-10 19:35:37 +0000

    make it better

--- server/index.js
+++ server/index.js
@@ -77,7 +77,7 @@ app.use('/api/socialmedialinks',sociallinksRoute)
 app.use('/api/contacts',contactsRoute);
 
 //Home Routes
- app.get('/',(req,res)=>{
+ app.get('/version',(req,res)=>{
      res.send('IntizAfriq Web App  version 0.1')
  })
 const options={ useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false }
