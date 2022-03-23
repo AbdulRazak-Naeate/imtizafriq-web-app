1990a628bba0053cbaeb8a0d121edabfc1af795e
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-23 08:40:13 +0000

    changing herosection slider item to form img to card

--- client/src/dashboard/pages/home/Home.jsx
+++ client/src/dashboard/pages/home/Home.jsx
@@ -7,12 +7,12 @@ import Widgetsm from "../../components/widgetSm/widgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 function Home({products,transactions,completedAggregate,inCompletedAggregate,alltimeAggregate,monthlySales}) {
     const alltimeAggregateDefault =[ { "_id": "0", "count": 0, "total": 0  } ]
-    const inCompleteAggregateDefualt =[{"_id": "0","count": 5, "total": 920}]
+    const inCompleteAggregateDefualt =[{"_id": "0","count": 0, "total": 0}]
     return (
         <div className="home">
        <div className="widgetwrapper">
                  {alltimeAggregate.length >0 ?  <FeaturedInfo completedAggregate={completedAggregate} inCompletedAggregate={inCompletedAggregate} alltimeAggregate={[alltimeAggregateDefault]}/>: <FeaturedInfo completedAggregate={[]} inCompletedAggregate={inCompleteAggregateDefualt} alltimeAggregate={[]}/>}
-                 
+
             {monthlySales ?  <Chart data={monthlySales} title="Sales Analytics" grid datakey={"Monthly Sales"}/>:''}
           <div className="homeWidgets">
            <Widgetsm products={products}/>
