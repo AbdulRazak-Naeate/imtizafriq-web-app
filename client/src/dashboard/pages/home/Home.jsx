540faeabab488acf24e7725574abffd1091a9028
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-03-23 10:39:19 +0000

    changing herosection slider item to form img to card

--- client/src/dashboard/pages/home/Home.jsx
+++ client/src/dashboard/pages/home/Home.jsx
@@ -14,11 +14,12 @@ function Home({products,transactions,completedAggregate,inCompletedAggregate,all
 
     const alltimeAggregateDefault =[ { "_id": "0", "count": 0, "total": 0  } ]
     const inCompleteAggregateDefualt =[{"_id": "0","count": 0, "total": 0}]
+    const completeAggregateDefault =[{"_id": "0","count": 0, "total": 0}]
 
     return (
         <div className="home">
        <div className="widgetwrapper">
-                 {alltimeAggregate.length >0 ?  <FeaturedInfo completedAggregate={completedAggregate} inCompletedAggregate={inCompletedAggregate} alltimeAggregate={[alltimeAggregateDefault]}/>: <FeaturedInfo completedAggregate={[]} inCompletedAggregate={inCompleteAggregateDefualt} alltimeAggregate={[]}/>}
+                 {alltimeAggregate.length >0 ? <FeaturedInfo completedAggregate={completedAggregate.length > 0 ?completedAggregate :completeAggregateDefault} inCompletedAggregate={inCompletedAggregate>0 ?inCompletedAggregate : inCompleteAggregateDefualt} alltimeAggregate={alltimeAggregate.length > 0? alltimeAggregate : alltimeAggregateDefault}/>: ''}
 
             {monthlySales ?  <Chart data={monthlySales} title="Sales Analytics" grid datakey={"Monthly Sales"}/>:''}
           <div className="homeWidgets">
