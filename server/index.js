0b9fb7356425de4df5109de81032b1c4abcd9ffc
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-04-18 11:58:45 +0000

    make it better

--- server/index.js
+++ server/index.js
@@ -1,12 +1,36 @@
+const bodyParser = require('body-parser')
 const express = require('express');
+const app = express();
 const pino = require('express-pino-logger')();
 const mongoose = require('mongoose');
 require('dotenv/config');
 const dotenv = require('dotenv');
-const app = express();
+dotenv.config();
+
 const cors = require('cors');
 const path = require('path');
-const fs = require("fs");
+
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://imtiz.herokuapp.com'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+//MiddleWare
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(cors(corsOptions)); // package to allow connections from outisde domains
+app.use(pino);
+
 
 //Import Routes
 const productsRoute     = require('./routes/products');
@@ -27,31 +51,8 @@ const states            = require('./routes/world/states');
 const cities            = require('./routes/world/cities');
 const sociallinksRoute  = require('./routes/socialmedialinks');
 const contactsRoute     = require('./routes/contacts');
-const Product  = require('./models/Product');
 
-dotenv.config();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://imtizafriq.herokuapp.com', 'http://imtizafriq.herokuapp.com','https://imtizafriq.com']
-
-const indexPath  = path.resolve(__dirname, '../client/build', 'index.html');
-const index_templatePath  = path.resolve(__dirname, '../client/build', 'index.html');
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-//MiddleWare
-app.use(cors(corsOptions)); // package to allow connections from outisde domains
-app.use(express.json({ limit: '50mb' })); //body-parser alternate
-app.use(pino);
 
 // This endpoint is pinged every 5 mins by uptimerobot.com to prevent 
 // free services like Heroku and Now.sh from letting the app go to sleep.
@@ -82,14 +83,13 @@ app.use('/api/cities',cities);
 app.use('/api/socialmedialinks',sociallinksRoute)
 app.use('/api/contacts',contactsRoute);
 
-//Home Routes
- app.get('/version',(req,res)=>{
+/* //Home Routes
+ app.get('/',(req,res)=>{
      res.send('IntizAfriq Web App  version 0.1')
- })
+ }) */
 const options={ useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false }
-
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION,options)
+mongoose.connect(process.env.DB_CONNECTION, options)
 
     const db = mongoose.connection
     db.once('open', _ =>{
@@ -98,45 +98,17 @@ mongoose.connect(process.env.DB_CONNECTION,options)
     db.on('error',err =>{
         console.error('connection eror:',err)
      })
-     console.log(process.env.NODE_ENV)
-     if (process.env.NODE_ENV === 'production') {
+     if (process.env.NODE_ENV  === 'production') {
         // Serve any static files
         app.use(express.static(path.resolve(__dirname, '../client/build')));
       // Handle React routing, return all requests to React app
-       /*  app.get('*', function(req, res) {
+        app.get('*', function(req, res) {
           res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-        }); */
-
-      // here we serve the index.html page
-      app.get('/*', async (req,res)=>{
-    
-      fs.readFile(indexPath, 'utf8',async (err, htmlData) => {
-      if (err) {
-          console.error('Error during file reading', err);
-          return res.status(404).end()
-      }
-      // TODO get product info
-     try{
-      const product = await Product.findById({_id:'622e45c00056e500161b52e9'});
-      
-      htmlData = htmlData.replace('__META_OG_TITLE__', product.name)
-      .replace('__META_OG_DESCRIPTION__', product.description)
-      .replace('__META_DESCRIPTION__', product.description)
-      .replace('__META_OG_IMAGE__', product.image[0].secure_url)
-
-      fs.writeFileSync(indexPath,htmlData,{encoding:'utf8',flag:'w'})
-
-      return res.send(htmlData);
-  }catch(err){
-      res.json({message:err})
-  }
-      // TODO inject meta tags
-  });
- 
-});
+        });
       }  
 //Start lestening to the server
-app.set('PORT',  process.env.PORT||3001);
-app.listen(app.get('PORT'),()=>{
+app.set('PORT',  process.env.PORT || 3001);
+
+app.listen(app.get('PORT'),() => {
     console.log(`Server is running on ${app.get('PORT')}`);
 });
