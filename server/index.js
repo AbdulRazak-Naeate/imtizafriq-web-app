e507fd782b921e44514d64601e2dfad94196f0d0
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2022-04-20 14:33:49 +0000

    fix dynamic meta-tag

--- server/index.js
+++ server/index.js
@@ -1,41 +1,18 @@
-const bodyParser = require('body-parser')
 const express = require('express');
-const app = express();
 const pino = require('express-pino-logger')();
 const mongoose = require('mongoose');
 require('dotenv/config');
 const dotenv = require('dotenv');
-dotenv.config();
-const Product  = require('../models/Product');
+const app = express();
 const cors = require('cors');
 const path = require('path');
+const fs = require("fs");
+const Product =require('./models/Product');
+/* var mustacheExpress = require('mustache-express');
 
-const fs = require('fs');
-
-const indexPath  = path.resolve(__dirname, '..', '../client/build', 'index.html');
-
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://imtizafriq.herokuapp.com'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-//MiddleWare
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-app.use(cors(corsOptions)); // package to allow connections from outisde domains
-app.use(pino);
-
-
+app.engine('mustache',mustacheExpress());
+app.set('views', __dirname + '/views');
+app.set('view engine','mustache'); */
 //Import Routes
 const productsRoute     = require('./routes/products');
 const prefarestyleProductRoute= require('./routes/prefarestyle');
@@ -55,8 +32,27 @@ const states            = require('./routes/world/states');
 const cities            = require('./routes/world/cities');
 const sociallinksRoute  = require('./routes/socialmedialinks');
 const contactsRoute     = require('./routes/contacts');
+dotenv.config();
 
+const indexPath  = path.resolve(__dirname, '../client/build', 'index.html');
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://imtizafriq.herokuapp.com', 'http://imtizafriq.herokuapp.com','https://imtizafriq.com']
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+//MiddleWare
+app.use(cors(corsOptions)); // package to allow connections from outisde domains
+app.use(express.json({ limit: '50mb' })); //body-parser alternate
+app.use(pino);
 
 // This endpoint is pinged every 5 mins by uptimerobot.com to prevent 
 // free services like Heroku and Now.sh from letting the app go to sleep.
@@ -87,13 +83,15 @@ app.use('/api/cities',cities);
 app.use('/api/socialmedialinks',sociallinksRoute)
 app.use('/api/contacts',contactsRoute);
 
-/* //Home Routes
- app.get('/',(req,res)=>{
+
+//Home Routes
+ app.get('/version',(req,res)=>{
      res.send('IntizAfriq Web App  version 0.1')
- }) */
+ })
 const options={ useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false }
+
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, options)
+mongoose.connect(process.env.DB_COMMUNITY_CON,options)
 
     const db = mongoose.connection
     db.once('open', _ =>{
@@ -102,59 +100,20 @@ mongoose.connect(process.env.DB_CONNECTION, options)
     db.on('error',err =>{
         console.error('connection eror:',err)
      })
-    
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
-  
-// Handle React routing, return all requests to React app
-  app.get('*',async (req, res)=> {
-    console.log("query productid from any request "+ req.query.productId)
-   try{
-    var pid=req.query.productId
-    var product;
-      console.log("productid "+pid)
-       if (pid){
-              product = await Product.findById({_id:pid});
-        }else{
-        product={
-         name:'ImtizAfriq',
-         description:'mark of honor',
-         image:  [
-          {
-            secure_url : 'https://res.cloudinary.com/abdulrazakneate/image/upload/v1647199679/,'
-          },
-        ]
-    }
-  }
-    
-     fs.readFile(indexPath,'utf8',(err,htmlData)=>{
+     console.log(process.env.NODE_ENV)
+     if (process.env.NODE_ENV === 'production') {
+        // Serve any static files
+        app.use(express.static(path.resolve(__dirname, '../client/build'),
+        { maxAge: '30d' }));
+      // Handle React routing, return all requests to React app
+        app.get('*',async (req, res)=> {
        
-       if (err){
-         console.error("Error during file reading")
-         return res.status(404).end()
-
-       } 
-      
-       htmlData=htmlData.replace("<title>ImtizAfriq</title>",`<title>${product.name}</title>`)
-       .replace('__META_OG_TITLE__',product.name)
-       .replace('__META_OG_DESCRIPTION__',product.description)
-       .replace('__META_DESCRIPTION__',product.description)
-       .replace('__META_OG_URL__',`https://imtizafriq.herokuapp.com/proceedcheckout?productId=${product.productId}`)
-       .replace('__META_OG_IMAGE__',product.image[0].secure_url)
-        res.send(htmlData)
-     })
-     
-     }catch(err){
-     console.log(err)
-   }
-   
-    //res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
-}    
+          
+          res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+        });
+      }  
 //Start lestening to the server
-app.set('PORT',  process.env.PORT || 3001);
-
-app.listen(app.get('PORT'),() => {
+app.set('PORT',  process.env.PORT||3001);
+app.listen(app.get('PORT'),()=>{
     console.log(`Server is running on ${app.get('PORT')}`);
 });
